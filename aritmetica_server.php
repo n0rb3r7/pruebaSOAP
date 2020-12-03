db44f8349dc334401d78f40923841c53166f4dba
n0rb3r7 <n0rb3r7@outlook.com>
2020-12-03 19:47:55 -0300

    Subida de archivos

---
+++ aritmetica_server.php
@@ -0,0 +1,22 @@
+ <?php
+if (!extension_loaded("soap")) {
+    dl("php_soap.dll");
+}
+
+ini_set("soap.wsdl_cache_enabled", "0");
+$server = new SoapServer("aritmetica.wsdl");
+
+function sumar($operando1, $operando2)
+{
+    return $operando1 + $operando2;
+}
+
+function restar($operando1, $operando2)
+{
+    return $operando1 - $operando2;
+}
+
+$server->AddFunction("sumar");
+$server->AddFunction("restar");
+$server->handle();
+?>
---
+++ cliente.php
@@ -0,0 +1,14 @@
+<?php
+// echo phpinfo();
+try{
+ $clienteSOAP = new SoapClient('http://localhost/pruebadelonline/aritmetica.wsdl');
+ 
+ $resultado_suma = $clienteSOAP->sumar(2.7, 3.5);
+ // $resultado_resta = $clienteSOAP->restar(2.7, 3.5);
+ 
+ echo "la suma de 2.7 mas 3.5 es: " . $resultado_suma . "<br/>";
+ // echo "la diferencia de 2.7 menos 3.5 es: " . $resultado_resta . "<br/>";
+ 
+} catch(SoapFault $e){
+ var_dump($e);
+}
